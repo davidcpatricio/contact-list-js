5996a39a5be7b36eb8573b25ccc1c8d9c8247647
David Patrício <david.c.patricio@gmail.com>
2023-12-10 12:49:31 +0000

    Logging in users

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/loginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if (req.session.user) return res.render('logged-in');
+  return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -17,7 +18,7 @@ exports.register = async function(req, res) {
       return;
     };
 
-    req.flash('success', 'User successfully created!');
+    req.flash('success', 'Successfully registered!');
     req.session.save(function() {
       return res.redirect('back');
     });
@@ -26,3 +27,32 @@ exports.register = async function(req, res) {
     return res.render('404');
   };
 };
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
+    };
+
+    req.flash('success', 'Successfully logged in!');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  };
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+}
